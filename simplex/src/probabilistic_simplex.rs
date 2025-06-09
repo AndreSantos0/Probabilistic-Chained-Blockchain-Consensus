d7f1c0d55266f2c96b1ac5c7d70702447b32a396
André Santos <80883346+AndreSantos0@users.noreply.github.com>
2025-06-09 19:07:11 +0100

    Changes for experiments and performance analysis (stable Version).

--- simplex/src/probabilistic_simplex.rs
+++ simplex/src/probabilistic_simplex.rs
@@ -485,6 +485,7 @@ impl ProbabilisticSimplex {
         if !self.proposes.contains_key(&propose.content.iteration) && sender == leader {
             let iteration = self.iteration.load(Ordering::Acquire);
             let is_extendable = self.blockchain.is_extendable(&propose.content);
+            self.proposes.insert(propose.content.iteration, propose.content.clone());
             if propose.content.iteration == iteration {
                 if is_extendable && !self.is_timeout.load(Ordering::Acquire) {
                     let block = SimplexBlockHeader::from(&propose.content);
@@ -504,7 +505,7 @@ impl ProbabilisticSimplex {
                         }
                         self.handle_iteration_advance(dispatcher_queue_sender).await;
                         let _ = reset_timer_sender.send(()).await;
-                        return;
+                        //return;
                     }
                 }
             }
@@ -557,8 +558,6 @@ impl ProbabilisticSimplex {
                     }
                 }
             }
-
-            self.proposes.insert(propose.content.iteration, propose.content);
         }
     }
 
