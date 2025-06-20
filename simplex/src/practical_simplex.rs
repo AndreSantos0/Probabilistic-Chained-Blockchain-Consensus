edabfedfee7eaf2261e58cc574f921619c92cffe
André Santos <80883346+AndreSantos0@users.noreply.github.com>
2025-06-20 16:11:55 +0100

    Changes for experiments and performance analysis.

--- simplex/src/practical_simplex.rs
+++ simplex/src/practical_simplex.rs
@@ -610,6 +610,9 @@ impl PracticalSimplex {
     }
 
     fn get_notarized(&self, iteration: Iteration) -> Option<(&SimplexBlockHeader, &Vec<VoteSignature>)> {
+        if iteration > self.iteration.load(Ordering::Acquire) {
+            return None
+        }
         self.votes
             .iter()
             .find(|(header, signatures)|
